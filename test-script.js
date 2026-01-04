fe4b18d755f0663f72eb0b01dbad7e3c27f968b3
Dittchy <mohandev0116@gmail.com>
2026-01-04 08:03:35 +0000

    Deploying to gh-pages from @ Dittchy/Retro-Pomodoro-Timer-@38cd8ae3bbc2829905f2d5bab14e2c53c20b530d 🚀

--- test-script.js
+++
@@ -1,7 +0,0 @@
-try {
-    document.getElementById('s3').innerText = "PASS";
-    document.getElementById('s3').className = "ok";
-} catch (e) {
-    document.getElementById('s3').innerText = "ERROR: " + e.message;
-    document.getElementById('s3').className = "fail";
-}
